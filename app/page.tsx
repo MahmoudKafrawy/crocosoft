e2c039c49a595b50c582995cbb089d60fef54d91
mahmoud kafrawy <mahmoud.kafrawy96@gmail.com>
2024-05-14 20:03:48 +0300

    [feature] question dialog ui and react hook form [fix] tailwind dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,25 @@
+import { QuestionDialog } from "@/components/dialogs/QuestionDialog";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { questions } from "@/data/questions";
-import { AddNewDialog } from "@/dialogs/AddNewDialog";
 
 export default function Home() {
   return (
     <main className="space-y-4 container py-16">
       <header className="flex justify-between w-full">
         <h1 className="text-3xl font-medium">Quiz App</h1>
-        <AddNewDialog />
+        <QuestionDialog />
       </header>
       {questions.questions_answers.map(({ answers, text, id }) => (
         <Card key={id}>
-          <CardHeader>
+          <CardHeader className="flex-row justify-between items-center">
             <CardTitle>
               {/* assume id can be displayed as question number */}
-              {id}.{text}
+              {id} . {text}
             </CardTitle>
+            <Button variant={"ghost"} asChild>
+              <QuestionDialog question={{ id, text, answers }} />
+            </Button>
           </CardHeader>
           <CardContent className="grid grid-cols-2 gap-2">
             {answers.map(({ text, id }) => (
---
+++ components/dialogs/QuestionDialog.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Square, SquareCheckBig, Trash2 } from "lucide-react";
+import { useFieldArray, useForm } from "react-hook-form";
+
+export function QuestionDialog({ question }: { question?: any }) {
+  const isEditingMode = Boolean(question);
+  const { control, register, watch, reset } = useForm({ defaultValues: question || {} });
+  const { fields, append, remove, update } = useFieldArray({ control, name: "answers" });
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>{isEditingMode ? "Edit Question" : "Add Question"}</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogTitle className="pb-2">{isEditingMode ? `Edit Question ${question.text}` : "Add Question"}</DialogTitle>
+        <Label htmlFor="question">Question</Label>
+        <Input placeholder="Question" id="question" {...register("text")} />
+        <div className="py-2">
+          <div className="flex justify-between items-center w-full pb-2">
+            <Label>Answers</Label>
+            <Button variant={"ghost"} className="bg-gray-50" onClick={() => append({ is_true: false, text: "" })}>
+              + Add Answer
+            </Button>
+          </div>
+          <div className="flex flex-col gap-2">
+            {fields.map((field: any, index) => (
+              <div key={field.id} className="flex gap-2">
+                <Input placeholder="Question" {...register(`answers[${index}].text`)} />
+                <Button variant={"ghost"} onClick={() => update(index, { ...field, is_true: !field.is_true })}>
+                  {field.is_true ? <SquareCheckBig color="#00cc4e" /> : <Square color="#000000" />}
+                </Button>
+                <Button onClick={() => remove(index)} variant={"ghost"}>
+                  <Trash2 color="#ff0000" />
+                </Button>
+              </div>
+            ))}
+          </div>
+        </div>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant={"outline"} onClick={() => reset()}>
+              Cancel
+            </Button>
+          </DialogClose>
+          <Button>{isEditingMode ? "Edit" : "Add"}</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
